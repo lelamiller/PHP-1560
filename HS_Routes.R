7dc5dacf5c24f244acb1fee5afc23084ac2514fa
lelamiller <lela_miller@brown.edu>
2025-11-19 11:41:14 -0500

    Update HS_Routes.R

--- HS_Routes.R
+++ HS_Routes.R
@@ -11,3 +11,17 @@ HS_Routes <- function(ridership){
 }
 
 only_hs <- HS_Routes(ridership)
+
+#look at the distribution for routes in which kids are riding regularly
+hs_routes <- only_hs %>%
+  mutate(
+    Time = as.POSIXct(Time, format = "%m/%d/%y %H:%M"),
+    hour = as.numeric(format(Time, "%H")),
+    date = as.Date(Time)
+  ) %>%
+  group_by(Route, date) %>%
+  summarise(nstudents = n())
+
+#now I have a data frame of the number of students that ride each route in each day. Lets look at an example day, 
+
+
