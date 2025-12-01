22af1c03ec03e07e7292fff3dc6b7d0864221232
lelamiller <lela_miller@brown.edu>
2025-12-01 10:49:51 -0500

    finished function to determine otp on school routes vs non school routes

---
+++ HS_Route_OTP.R
@@ -0,0 +1,4 @@
+
+#read in the data
+otp <- read.csv("/Users/lelamiller/Downloads/otp_simulated.csv" )
+ridership <- read.csv("/Users/lelamiller/Downloads/ridership_simulated.csv")
--- HS_Routes.R
+++ HS_Routes.R
@@ -1,6 +1,8 @@
-#' @description determines the bus routes of most interest, as they appear to be used as transportation to and from school by the most students
+#' @description determines the bus routes of most interest, as they appear to be used as transportation to and from school by the most students.
+#' Also, determines the on time performance as a proportion of busses late by more than 5 minutes during school hours and outside of school hours. 
 #' @param HS_ridership_result data frame with the route, hour of day, and x_hat, the average number of student passengers on a week day
-#' @return appended HS_ridership_result, excluding routes that do not appear to be school bus routes
+#' @param otp a dataframe of bus routes, their arrival times and scheduled arrival times. Indicates the on time performance of busses. 
+#' @return otp_summary, a comparison of the proportion of late arrivals on school routes during school hours and non school hours
 
 
 #First metric to determine whether a bus is a school route or not:
@@ -10,11 +12,14 @@
   #2: On each route, we can compare average ridership during these hours to the average ridership during non-school hours
 
 HS_ridership_result <- read.csv("/Users/lelamiller/Documents/GitHub/PHP-1560/Data/HS_ridership_result.csv")
+otp <- read.csv("/Users/lelamiller/Downloads/otp_simulated.csv" )
 
-HS_Routes <- function(HS_ridership_result){
+library(tidyverse)
+
+school_hours <- c(6, 7, 14, 15)
+
+HS_Routes <- function(HS_ridership_result, otp, school_hours){
   
-  school_hours <- c(6, 7, 14, 15)
-
 #comparing school hour riders to non school hours riders
   school_vs_nonschool <- HS_ridership_result %>%
     mutate(commute = ifelse(hour %in% school_hours, "school_commute", "other")) %>%
@@ -38,7 +43,30 @@ HS_Routes <- function(HS_ridership_result){
     filter(likely_school_route == TRUE) %>%
     select(Route)
   
-  return(school_routes)
+  school_routes <- school_routes$Route
+  
+#Now, filter the on time performance data to be those routes
+  otp_summary <- otp %>%
+    filter(Route %in% school_routes) %>%
+    #mutate to add columns for hours, indicator for school hours, and indicator for late
+    mutate(
+      Scheduled.Time = as.POSIXct(Scheduled.Time, format = "%Y-%m-%d %H:%M:%S"),
+      hour = as.numeric(format(Scheduled.Time, "%H")),
+      Late = ifelse(Delay.Sec > 300, "Late", "On Time/Early"),
+      school_period = ifelse(hour %in% school_hours, "school_hours", "nonschool_hours")
+    ) %>%
+    #find the number and proportion late and on time by whether or not its during school hours
+    group_by(school_period, Late) %>%
+    summarize(n = n(), .groups = "drop") %>%
+    group_by(school_period) %>%
+    mutate(prop = n / sum(n)) %>%
+    pivot_wider(
+      names_from = Late,
+      values_from = prop,
+      names_prefix = "prop_"
+    )
+
+  return(otp_summary)
 }
 
     
