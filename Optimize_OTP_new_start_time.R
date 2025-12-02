e3910b81f3fdc76b6c9be180fc90285b0d8a60b1
lelamiller <lela_miller@brown.edu>
2025-12-02 09:46:05 -0500

    Update Optimize_OTP_new_start_time.R

--- Optimize_OTP_new_start_time.R
+++ Optimize_OTP_new_start_time.R
@@ -2,7 +2,7 @@
 #' optimizes on time performance for an input of school hours based
 #' on stops that are most popular among students during current school times. 
 #' recommends a new route or to keep a route for the students commute to school 
-#' based on the route with the best otp for a stop during that hour. 
+#' based on the route with the best otp for a stop during that hour. For the 50 most popular bus stops
 #' @param ridership data frame with the route, type of rider (High.School), 
 #' @param otp a data frame of bus routes, their arrival times and scheduled arrival times. Indicates the on time performance of busses. 
 #' @param school_hours a vector of hours that students would be riding the bus based on school start time and release time
@@ -15,14 +15,17 @@ ridership <- read.csv("/Users/lelamiller/Downloads/ridership_simulated.csv")
 #MAP FOR STOPS:
 stops <- read.delim("/Users/lelamiller/Documents/GitHub/PHP-1560/Data/stops.txt", sep = ",")
 
-new_school_hours <- c(6, 7, 14, 15)
+new_school_morning <- c(6, 7)
+new_school_afternoon <- c(14, 15)
   #include hour before start time, hour during start time
   #include hour of end time and hour after end time
-
+old_school_morning <- c(6, 7)
+old_school_afternoon <- c(14, 15)
+  
 Route_allocation <- function(otp, ridership, school_hours) {
   
 #look at the popular stops on the hs routes, how do these overlap between routes, how can we change routes based on otp
-routes_stops <- ridership %>%
+morning_stops <- ridership %>%
   filter(High.School == "Providence Public School Department") %>%
   filter(Day.of.Week != "Sat", 
          Day.of.Week != "Sun") %>%
@@ -31,12 +34,28 @@ routes_stops <- ridership %>%
     hour = as.numeric(format(Time, "%H")),
     date = as.Date(Time)
   ) %>%
-  filter(hour %in% school_hours) %>%
+  filter(hour %in% old_school_morning) %>%
   group_by(Stop.Number, Route) %>%
   summarize(nstudents = n()) %>%
-  arrange(desc(nstudents))
+  arrange(desc(nstudents)) %>%
+  group_by(Stop.Number)
 
-print(head(routes_stops))
+afternoon_stops <- ridership %>%
+  filter(High.School == "Providence Public School Department") %>%
+  filter(Day.of.Week != "Sat", 
+         Day.of.Week != "Sun") %>%
+  mutate(
+    Time = as.POSIXct(Time, format = "%m/%d/%y %H:%M"),
+    hour = as.numeric(format(Time, "%H")),
+    date = as.Date(Time)
+  ) %>%
+  filter(hour %in% old_school_afternoon) %>%
+  group_by(Stop.Number, Route) %>%
+  summarize(nstudents = n()) %>%
+  arrange(desc(nstudents)) %>%
+  group_by(Stop.Number)
+
+
 
 #use the map of stops to add stop_id to the otp dataframe, this will allow us to compare the routes by the stops the students ride
 stops <- stops %>%
@@ -52,14 +71,7 @@ otp <- otp %>%
     Late = ifelse(Delay.Sec > 300, 1, 0)    # flag lateness > 5 min
   )
 
-otp_stop_perf <- otp %>%
-  filter(hour %in% school_hours) %>%
-  group_by(stop_id, Route) %>%
-  summarize(
-    mean_delay = mean(Delay.Sec, na.rm = TRUE),
-    late_rate = mean(Late, na.rm = TRUE),
-    .groups = "drop"
-  )
+
 
 
 #maybe a loop through most popular stops, to find the route with the best on time performance for that stop during the hour?
